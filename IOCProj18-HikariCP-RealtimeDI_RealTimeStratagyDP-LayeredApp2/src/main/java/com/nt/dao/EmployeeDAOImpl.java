675cab964ae2e9ab5d0128dfd643ac24098e9a0e
hp <hp@DESKTOP-S88HL35>
2020-08-07 15:16:53 +0530

    initial import By K.Govardhan #<A journey of a thousand miles begins with a single step>

---
+++ IOCProj18-HikariCP-RealtimeDI_RealTimeStratagyDP-LayeredApp2/src/main/java/com/nt/dao/EmployeeDAOImpl.java
@@ -0,0 +1,38 @@
+package com.nt.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.sql.DataSource;
+
+import com.nt.bo.EmployeeBO;;
+
+public final class EmployeeDAOImpl implements EmployeeDAO {
+	 private static final String EMP_INSERT_QUERY=
+			 "INSERT INTO EMPLOYEE_SPRING1 VALUES(ENO_SEQ1.NEXTVAL,?,?,?,?,?)";
+	private DataSource ds;
+	 // Alt+shift+s,0(con)
+	public EmployeeDAOImpl(DataSource ds) {
+		this.ds = ds;
+	}
+
+	@Override
+	public int insert(EmployeeBO bo) throws Exception {
+		Connection con=null;
+		PreparedStatement ps1 =null;
+		int count1=0;
+		con=ds.getConnection();
+		System.out.println("EmployeeDAOImpl.insert()"+ds.getClass());
+		ps1=con.prepareStatement(EMP_INSERT_QUERY);
+		ps1.setString(1,(bo.geteName()));
+		ps1.setString(2,(bo.geteAdd()));
+		ps1.setFloat(3, (bo.geteBasicSalary()));
+		ps1.setFloat(4,(bo.geteGroasssalary()));
+		ps1.setFloat(5,(bo.geteNetSalary()));
+		count1=ps1.executeUpdate();
+				ps1.close();
+				con.close();
+		return count1;
+	}
+
+}
---
+++ IOCProj18-HikariCP-RealtimeDI_RealTimeStratagyDP-LayeredApp2/src/main/java/com/nt/vo/EmployeeVO.java
@@ -0,0 +1,44 @@
+package com.nt.vo;
+
+public class EmployeeVO {
+
+	private String eName;
+	private String eId;
+	private String eAdd;
+	private String eBasicSalary;
+
+	// Alt+shift+s,r(S&G methods)
+	
+	public String geteName() {
+		return eName;
+	}
+
+	public void seteName(String eName) {
+		this.eName = eName;
+	}
+
+	public String geteId() {
+		return eId;
+	}
+
+	public void seteId(String eId) {
+		this.eId = eId;
+	}
+
+	public String geteAdd() {
+		return eAdd;
+	}
+
+	public void seteAdd(String eAdd) {
+		this.eAdd = eAdd;
+	}
+
+	public String geteBasicSalary() {
+		return eBasicSalary;
+	}
+
+	public void seteBasicSalary(String eBasicSalary) {
+		this.eBasicSalary = eBasicSalary;
+	}
+
+}
